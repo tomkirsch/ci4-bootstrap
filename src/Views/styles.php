2f01b8ee582fab4c02c8a3c35cfedf58b142198b
tomkirsch <mrmotts@gmail.com>
2022-11-29 21:20:49 -0500

    bug

--- src/Views/styles.php
+++ src/Views/styles.php
@@ -59,6 +59,7 @@
             position: absolute;
             top: 0;
             left: 0;
+            width: 100%;
             height: 100%;
             display: flex;
             align-items: center;
